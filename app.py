e82f554fcb7ae1c16776626a78aefa3f1036c85b
VinceRF <97954474+VinceRF@users.noreply.github.com>
2022-05-12 17:04:45 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('localhost', 27017) #로컬에서 할때
-#client = MongoClient('mongodb://test:test@localhost', 27017) # AC2 로 공유할때
+# client = MongoClient('localhost', 27017)
+client = MongoClient('52.79.236.11', 27017, username="test", password="test")
 db = client.dbsparta_plus_week4
 
 import requests
+
+
 
 
 @app.route('/')
@@ -67,7 +69,7 @@ def sign_in():
             'id': username_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')  # .decode('utf-8') #로컬시 주석차리
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256') #.decode('utf-8') #로컬시 주석차리
 
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
@@ -77,7 +79,6 @@ def sign_in():
 
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
-    # 회원가입
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
@@ -93,9 +94,9 @@ def sign_up():
     return jsonify({'result': 'success'})
 
 
+
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    #ID 중복체크
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     # print(value_receive, type_receive, exists)
@@ -179,7 +180,6 @@ def get_posts():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        #DB에서 데이터 가져오기
         my_username = payload["id"]
         username_receive = request.args.get("username_give")
         if username_receive == "":
